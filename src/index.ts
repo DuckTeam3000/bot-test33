297dc4dbfd8b6d28cc71b601a688b96a77ec2514
dhlee <dhlee@co.kr>
2025-07-21 01:39:51 +0900

    get

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!"); 
+import * as https from 'https';
+
+console.log("Hello, World!");
+
+https.get('https://www.google.com', (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log('Response body length:', data.length);
+    // console.log(data); // Uncomment this line if you want to see the full HTML response
+  });
+
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+}); 
